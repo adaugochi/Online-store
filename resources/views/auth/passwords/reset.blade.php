df29e1b3e5283486a0e5da1383fbc40d7c806ce0
Adaa <adaamgbede@gmail.com>
2021-08-15 16:07:59 +0100

    design auth pages

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,31 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
+@section('header', 'Reset Password')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="form-input">
+                <input name="email" type="text" placeholder="Your Email *">
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="form-input">
+                <input name="password" type="text" placeholder="Your Password *">
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="form-input">
+                <input name="password" type="text" placeholder="Your Confirm password *">
+            </div>
+        </div>
+        <div class="col-12">
+            <button class="btn btn--primary btn-block" type="submit">
+                Reset
+            </button>
+            <div class="mt-5 text-center fs-14">
+                Not interested?
+                <a href="{{ url('register') }}" class="text-primary font-weight-bold"> Sign In</a>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,22 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
+@section('header', 'Verify Your Email Address')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
-                </div>
+    <div class="row">
+        <div class="col-12">
+            <p>Before proceeding, please check your email for a verification link.</p>
+            <p>If you did not receive the email, click on the button below to request for another</p>
+        </div>
+        <div class="col-12">
+            <button class="btn btn--primary btn-block" type="submit">
+                click here
+            </button>
+        </div>
+        <div class="col-12">
+            <div class="mt-5 text-center fs-14">
+                Not interested?
+                <a href="{{ url('register') }}" class="text-primary font-weight-bold">Sign In</a>
             </div>
         </div>
     </div>
-</div>
 @endsection
