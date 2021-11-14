e69332a0ccf19e52a692896bcb89de8040099778
adaa <adaamgbede@gmail.com>
2021-11-15 00:59:08 +0100

    update

--- database/migrations/2021_08_28_141555_create_user_verifications_table.php
+++ database/migrations/2021_08_28_141555_create_user_verifications_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUserVerificationsTable extends Migration
@@ -19,7 +20,7 @@ class CreateUserVerificationsTable extends Migration
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('verification_code');
             $table->timestamp('expires_at');
-            $table->timestamp('created_at');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
